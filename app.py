db8c8c2aa4fabeaf746d81b652391ba55caed2fa
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-10-07 22:03:36 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -253,7 +253,6 @@ def a2():
     return 'со слэшем'
 
 flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
-
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
     if flower_id >= len(flower_list):
