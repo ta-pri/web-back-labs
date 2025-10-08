98b60fc5ec059a1f0be685500bea3a17f29f7def
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-10-08 17:57:14 +0700

    Имя студента

--- app.py
+++ app.py
@@ -277,4 +277,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Таисия Приваловa'
+    return render_template('example.html', name=name)
