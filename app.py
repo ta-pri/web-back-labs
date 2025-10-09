064e848765ea6de6fd9274ab90e8610ac6a6e2ab
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-10-10 00:43:33 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -288,3 +288,7 @@ def example():
   return render_template('example.html',
                          name=name, lab=lab, grup=grup,
                          curs=curs, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
