6e2278a9fc9e1477517fbf58e0a32492ebfa00e9
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-10-11 11:22:24 +0700

    Рефакторинг: выделение первой лабораторной

---
+++ lab1.py
@@ -0,0 +1,181 @@
+from flask import Blueprint, url_for, request, redirect
+import datetime
+lab1 = Blueprint('lab1',__name__)
+
+
+@lab1.route("/lab1/web")
+def web():
+    return """<!doctype html>
+        <html>
+           <body>
+               <h1>web-сервер на flask</h1>
+               <a href="/author">author</a>
+           </body>
+        </html>""", 200, {
+            "X-Server": "sample",
+            "Content-Type": "text/plain; charset=utf-8" 
+        }
+
+
+@lab1. route("/lab1/author")
+def author():
+    name = "Привалова Таисия Дмитриевна"
+    group = "ФБИ-33"
+    faculty = "ФБ"
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """ </p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
+
+
+@lab1.route('/lab1/image') 
+def image():
+    path = url_for("static", filename="oak.jpg")
+    css = url_for("static", filename="lab1.css")
+    return f'''
+<html>
+    <head>
+        <link rel="stylesheet" href="{css}">
+    </head>
+    <body>
+        <h1>Дуб</h1>
+        <img src="{path}">
+    </body>
+</html>
+''', 200, {
+    "Content-Type": "text/html; charset=utf-8",
+    "Content-Language": "ru-RU",            
+    "X-My-Header-1": "Privet",          
+    "X-My-Header-2": "12345"           
+}
+
+
+count = 0
+
+
+@lab1.route("/lab1/counter")
+def counter():
+    global count
+    count += 1
+    time = datetime.datetime.today()
+    url = request.url
+    client_ip = request.remote_addr
+    return '''
+<!doctype html>
+<html>
+   <body>
+      Сколько раз вы сюда заходили ''' + str(count) + '''
+      <hr>
+      Дата и время: ''' + str(time) + '''<br>
+      Запрошенный адрес: ''' + url + '''<br>
+      Ваш IP-адрес: ''' + client_ip + '''<br>
+      <a href='/lab1/reset_counter'>Сбросить счётчик</a>
+  </body>
+</html>
+'''
+
+
+@lab1.route("/lab1/reset_counter")
+def reset_counter():
+    global count
+    count = 0
+    return redirect("/lab1/counter")
+
+
+@lab1.route("/lab1/info")
+def info():
+    return redirect("/lab1/author")
+
+
+@lab1.route("/lab1/created")
+def created():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Создано успешно</h1>
+        <div><i>что-то создано...</i></div>
+    </body>
+</html>
+''', 201
+
+
+@lab1.route("/lab1")
+def lab():
+    return """
+<!doctype html>
+<html>
+   <head>
+      <title>Лабораторная 1</title>
+   </head>
+   <body>
+      Flask — фреймворк для создания веб-приложений на языке
+      программирования Python, использующий набор инструментов
+      Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+      называемых микрофреймворков — минималистичных каркасов веб-приложений, 
+      сознательно предоставляющих лишь самые базовые возможности.
+      <br><br>
+      <a href='/'>На главную</a>
+      <h2>Список роутов</h2>
+      <ul>
+         <li><a href='/lab1/web'>/lab1/web</a></li>
+         <li><a href='/lab1/author'>/lab1/author</a></li>
+         <li><a href='/lab1/image'>/lab1/image</a></li>
+         <li><a href='/lab1/counter'>/lab1/counter</a></li>
+         <li><a href='/lab1/reset_counter'>/lab1/reset_counter</a></li>
+         <li><a href='/lab1/info'>/lab1/info</a></li>
+         <li><a href='/lab1/created'>/lab1/created</a></li>
+         <li><a href='/lab1/400'>/lab1/400</a></li>
+         <li><a href='/lab1/401'>/lab1/401</a></li>
+         <li><a href='/lab1/402'>/lab1/402</a></li>
+         <li><a href='/lab1/403'>/lab1/403</a></li>
+         <li><a href='/lab1/405'>/lab1/405</a></li>
+         <li><a href='/lab1/418'>/lab1/418</a></li>
+         <li><a href='/lab1/404'>/lab1/404</a></li>
+         <li><a href='/lab1/error500'>/lab1/error500</a></li>
+      </ul>
+   </body>
+</html>
+"""
+
+
+@lab1.route("/lab1/400")
+def error_400():
+    return "<h1>400 Bad Request</h1><p>Неверный запрос</p>", 400
+
+
+@lab1.route("/lab1/401")
+def error_401():
+    return "<h1>401 Unauthorized</h1><p>Требуется авторизация</p>", 401
+
+
+@lab1.route("/lab1/402")
+def error_402():
+    return "<h1>402 Payment Required</h1><p>Требуется оплата</p>", 402
+
+
+@lab1.route("/lab1/403")
+def error_403():
+    return "<h1>403 Forbidden</h1><p>Доступ запрещён</p>", 403
+
+
+@lab1.route("/lab1/405")
+def error_405():
+    return "<h1>405 Method Not Allowed</h1><p>Метод не разрешён</p>", 405
+
+
+@lab1.route("/lab1/418")
+def error_418():
+    return "<h1>418 I'm a teapot</h1><p>Я чайник </p>", 418
+
+
+@lab1.route("/lab1/error500")
+def error_500():
+    x = 1 / 0
+    return f"Результат: {x}" 
+
