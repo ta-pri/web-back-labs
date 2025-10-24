b94db403a6d295757be57c8c9dcfa5f00e27d2f7
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-10-24 23:22:31 +0700

    Очистка кук

--- lab3.py
+++ lab3.py
@@ -159,3 +159,13 @@ def train_ticket():
                            luggage=luggage, age=age, from_city=from_city,
                            to_city=to_city, date=date, insurance=insurance,
                            price=price, ticket_type=ticket_type)
+
+
+@lab3.route('/lab3/clear_settings')
+def clear_settings():
+    resp = make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('font_style')
+    return resp
