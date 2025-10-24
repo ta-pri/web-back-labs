5331f5fbe16529d17d7fc3837ea7a451e3e3a464
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-10-24 18:47:49 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -41,3 +41,34 @@ def form1():
     return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors, errors1=errors1)
 
 
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    #Пусть кофе стоит 120 р, черный чай 80р, зеленый 70р
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    #Добавка молока удорожает на 30 р, а сахара на 10р
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('lab3/pay.html', price=price)
+
+
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price')
+    return render_template('lab3/success.html', price=price)
