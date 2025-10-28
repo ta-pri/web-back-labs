585ce7210b00cc41b680858cde5177f9994692da
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-10-29 00:07:14 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 import datetime
 
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 
 log_404 = []
@@ -61,6 +63,7 @@ def index():
          <li><a href='/lab1'>Первая лабораторная</a></li>
          <li><a href="/lab2">Вторая лабораторная</a></li>
          <li><a href="/lab3">Третья лабораторная</a></li>
+         <li><a href="/lab4">Четвертая лабораторная</a></li>
       </ul>
       <hr>
       <footer>
