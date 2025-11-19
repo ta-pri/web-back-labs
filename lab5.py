0998eaaf1c273f2c51dcb5b989a99fd8e96be4f8
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-11-20 01:17:36 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import psycopg2
 
 lab5 = Blueprint('lab5', __name__)
 
@@ -6,3 +7,36 @@ lab5 = Blueprint('lab5', __name__)
 def main():
     username = "Anonymous"
     return render_template('lab5/lab5.html', username=username)
+
+
+@lab5.route('/lab5/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not login or not password:
+        return render_template('lab5/register.html', error='Заполните все поля')
+    
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'taisiia_privalova_knowledge_base',
+        user = 'taisiia_privalova_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template('lab5/register.html',
+                               error="Такой пользователь уже существует")
+    
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html', login=login)
