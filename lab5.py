fc7f654c6cd2b4b4a40ed2f820446ade6ed06415
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-11-21 00:36:10 +0700

    Удаление дублирующегося кода

--- lab5.py
+++ lab5.py
@@ -10,6 +10,24 @@ def main():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
 
+def db_connect():
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'kb',
+        user = 'taisiia_privalova_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    return conn, cur
+
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -21,63 +39,46 @@ def register():
     if not login or not password:
         return render_template('lab5/register.html', error='Заполните все поля')
     
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'taisiia_privalova_knowledge_base',
-        user = 'taisiia_privalova_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor()
+    conn, cur = db_connect()
 
     cur.execute(f"SELECT login FROM users WHERE login='{login}';")
     if cur.fetchone():
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/register.html',
                                error="Такой пользователь уже существует")
     
     cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
-    conn.commit()
-    cur.close()
-    conn.close()
+    
+    db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
 
-@lab5.route('/lab5/login', methods = ['GET', 'POST'])
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('lab5/login.html')
-    
-    login = request.form.get('login')
+
+    login_value = request.form.get('login')
     password = request.form.get('password')
 
-    if not login or not password:
+    if not login_value or not password:
         return render_template('lab5/login.html', error="Заполните поля")
-    
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'taisiia_privalova_knowledge_base',
-        user = 'taisiia_privalova_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor(cursor_factory=RealDictCursor)
 
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    conn, cur = db_connect()
+
+    cur.execute(f"SELECT * FROM users WHERE login='{login_value}';")
     user = cur.fetchone()
 
     if not user:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html',
-                               error='Логин и/или пароль неверны')
-    
+                               error="Логин и/или пароль неверны")
+
     if user['password'] != password:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html',
-                               error='Логин и/или пароль неверны')
-    
-    session['login'] = login
-    cur.close()
-    conn.close()
-    return render_template('lab5/success_login.html', login=login)
+                               error="Логин и/или пароль неверны")
+
+    session['login'] = login_value
+    db_close(conn, cur)
+    return render_template('lab5/success_login.html', login=login_value)
