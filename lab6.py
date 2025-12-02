28a6520d77a5e6477af3056cbe1e9eba624335e8
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-12-02 18:40:18 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -51,7 +51,39 @@ def api():
                     'result': 'success',
                     'id': id
                 }
-    
+            
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office not booked'
+                        },
+                        'id': id
+                    }
+
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'Forbidden'
+                        },
+                        'id': id
+                    }
+
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+            
     return {
         'jsonrpc': '2.0',
         'error': {      
