9138b0d92665d53e8a4132b7ec9eee369d64bd46
Таисия Привалова <ta_privalova.ru@mail.ru>
2025-12-31 01:10:34 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 from rgz import rgz
 
@@ -56,6 +57,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 app.register_blueprint(rgz)
 
@@ -114,6 +116,7 @@ def index():
          <li><a href="/lab6">Шестая лабораторная</a></li>
          <li><a href="/lab7">Седьмая лабораторная</a></li>
          <li><a href="/lab8">Восьмая лабораторная</a></li>
+         <li><a href="/lab9">Девятая лабораторная</a></li>
          <li><a href="/rgz/">РГЗ (Книги)</a></li>
       </ul>
       <hr>
